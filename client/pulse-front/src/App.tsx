0253f1af5c58656947e784d47954166e91366d35
Stefan Schaerdinger <stefan.schaerdinger@tum.de>
2025-05-17 17:00:56 +0200

    Setting up skeleton landing page

--- client/pulse-front/src/App.tsx
+++ client/pulse-front/src/App.tsx
@@ -3,7 +3,41 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from "@/components/ui/button"
+import { useEffect, useRef } from "react";
 import {ProfileForm } from "@/elements/landingform"
+
+
+
+
+
+
+export function LandingPage() {
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 via-purple-50 to-pink-50">
+      {/* Main Content (centered) */}
+      <div className="flex flex-col items-center justify-center h-screen text-center px-4">
+        <h1 className="text-3xl font-bold mb-3 text-gray-900">
+        We help diverse teams keep up with others changes
+        </h1>
+        <p className="text-xl text-gray-600 max-w-md">
+          Generate summaries from your weekly code progress with our LLM
+        </p>
+      </div>
+
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <ProfileForm></ProfileForm>
+      </div>
+
+    </div>
+  );
+}
+
+
+
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -36,14 +70,10 @@ function App() {
       <Button>Click me</Button>
       </div> */}
 
-      <div className="flex flex-col items-center justify-center min-h-svh">
-        <ProfileForm></ProfileForm>
-      </div>
 
-      
 
     </>
   )
 }
 
-export default App
+export default LandingPage
