c3caa053c6c6ad375647fc22dbbb6dae4700c740
Frank Elsinga <frank@elsinga.de>
2025-05-26 02:53:34 +0200

    add metrics

--- server/src/main/java/com/autoback/autoback/api/RESTController.java
+++ server/src/main/java/com/autoback/autoback/api/RESTController.java
@@ -4,6 +4,8 @@ import com.autoback.autoback.CommunicationObjects.LinkConstruct;
 import com.autoback.autoback.CommunicationObjects.PATConstruct;
 import com.autoback.autoback.CommunicationObjects.UserCodeConstruct;
 import com.autoback.autoback.CommunicationObjects.UserConstruct;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,33 +14,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.Buffer;
-import java.nio.ByteBuffer;
-import java.util.*;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class RESTController {
 
     private final ServiceRest serviceRest;
-    // will be replaced by db
-    private Map<String, String> repo2patmapping = new HashMap<>();
-    private Map<LinkConstruct, String> linkconstruct2repomapping = new HashMap<LinkConstruct, String>();
-
+    private final Counter patRegistrationCnt;
 
     @Autowired
-    public RESTController(ServiceRest sr){
-
+    public RESTController(ServiceRest sr, MeterRegistry registry) {
         serviceRest = sr;
-
+        patRegistrationCnt = Counter.builder("pat_registration_total").description("Total number of personal access tokens registered").register(registry);
     }
 
-
     @PostMapping("/providePAT")
-    public ResponseEntity<LinkConstruct> registerPAT(@RequestBody PATConstruct patrequest){
+    public ResponseEntity<LinkConstruct> registerPAT(@RequestBody PATConstruct patrequest) {
 
         Optional<LinkConstruct> lc = serviceRest.getLinkConstruct(patrequest);
         if (lc.isPresent()) {
+            patRegistrationCnt.increment();
             return ResponseEntity.status(HttpStatus.OK).body(lc.get());
         } else {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
@@ -46,7 +42,7 @@ public class RESTController {
     }
 
     @PostMapping("/uuidmappingtodata")
-    public ResponseEntity<UserConstruct> uuidMappingToPersoData(@RequestBody UserCodeConstruct userCodeConstruct){
+    public ResponseEntity<UserConstruct> uuidMappingToPersoData(@RequestBody UserCodeConstruct userCodeConstruct) {
         // responds with the repo and role the uuid is referring to
         // it does so by decomposing the uuidcode into:
         // uuidcode <-> patternpadding(repouuid,role)
@@ -60,47 +56,5 @@ public class RESTController {
         } else {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
-
-
-        
     }
-
-    public static void main(String[] args){
-        /*
-        int dbuuid = 243232;
-
-        ByteBuffer bb = ByteBuffer.allocate(16).putInt(dbuuid);
-
-        bb.put((byte) 100); // 100 for developer
-
-        byte[] bbb = bb.array();
-        String asdf = Base64.getEncoder().encodeToString(bbb);
-        System.out.println(asdf);
-        */
-
-        UUID uuid = UUID.randomUUID();
-
-        String uuidstring = uuid.toString();
-
-
-        uuidstring = uuidstring.concat("_");
-        uuidstring = uuidstring.concat("100");
-
-
-        String rgx = "_";
-        String[] asdf = uuidstring.split(rgx);
-
-
-        System.out.println(asdf[0]);
-
-
-
-
-
-
-    }
-
-    
-
-
 }
