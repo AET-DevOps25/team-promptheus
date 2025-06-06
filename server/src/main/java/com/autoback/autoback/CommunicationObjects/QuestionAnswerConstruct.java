8b482f429f9e8185abe5cdba838bf624f93ab75c
Frank Elsinga <frank@elsinga.de>
2025-06-06 03:05:03 +0200

    fix answering

--- server/src/main/java/com/autoback/autoback/CommunicationObjects/QuestionAnswerConstruct.java
+++ server/src/main/java/com/autoback/autoback/CommunicationObjects/QuestionAnswerConstruct.java
@@ -1,7 +1,15 @@
 package com.autoback.autoback.CommunicationObjects;
 
+import com.autoback.autoback.persistence.entity.QuestionAnswer;
+import jakarta.persistence.Column;
 import lombok.Builder;
 
+import java.time.Instant;
+
 @Builder
-public record QuestionAnswerConstruct() {
+public record QuestionAnswerConstruct(String answer, Instant createdAt) {
+
+    public static QuestionAnswerConstruct from(QuestionAnswer a) {
+        return QuestionAnswerConstruct.builder().answer(a.getAnswer()).createdAt(a.getCreatedAt()).build();
+    }
 }
--- server/src/main/java/com/autoback/autoback/CommunicationObjects/QuestionConstruct.java
+++ server/src/main/java/com/autoback/autoback/CommunicationObjects/QuestionConstruct.java
@@ -9,7 +9,7 @@ import java.util.List;
 @Builder
 public record QuestionConstruct(String question,Instant createdAt,List<QuestionAnswerConstruct> answers) {
     public static QuestionConstruct from(Question q) {
-        List<QuestionAnswerConstruct> answers = q.getAnswers().stream().map(a -> QuestionAnswerConstruct.builder().build()).toList();
+        List<QuestionAnswerConstruct> answers = q.getAnswers().stream().map(QuestionAnswerConstruct::from).toList();
         return QuestionConstruct.builder().question(q.getQuestion()).createdAt(q.getCreatedAt()).answers(answers).build();
     }
 }
