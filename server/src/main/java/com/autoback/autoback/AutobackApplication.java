bc7fcca38a2018bb7d07f8dd66d66103078ebff4
Frank Elsinga <frank@elsinga.de>
2025-06-09 12:08:55 +0200

    move logging to another PR

--- server/src/main/java/com/autoback/autoback/AutobackApplication.java
+++ server/src/main/java/com/autoback/autoback/AutobackApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,8 +15,6 @@ import java.util.Map;
 @CrossOrigin(origins = "*")
 public class AutobackApplication {
 	public static void main(String[] args) {
-    	OpenTelemetrySdk openTelemetrySdk = AutoConfiguredOpenTelemetrySdk.initialize().getOpenTelemetrySdk()
-        OpenTelemetryAppender.install(openTelemetrySdk);
 		SpringApplication.run(AutobackApplication.class, args);
 	}
 }
