abde3078524749cdfb963e352a833b5c8052a4b5
Stefan Schaerdinger <stefan.schaerdinger@tum.de>
2025-06-11 15:02:41 +0200

    Rebase to current new-server

---
+++ server/src/main/java/com/autoback/autoback/api/GitRepoController.java
@@ -0,0 +1,127 @@
+package com.autoback.autoback.api;
+
+import com.autoback.autoback.CommunicationObjects.*;
+import com.meilisearch.sdk.model.SearchResult;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/repositories")
+public class GitRepoController {
+    private final GitRepoService gitRepoService;
+    private final SearchService searchService;
+    private final MeterRegistry meterRegistry;
+
+    @Autowired
+    public GitRepoController(GitRepoService gitRepoService, SearchService searchService, MeterRegistry registry) {
+        this.gitRepoService = gitRepoService;
+        this.searchService = searchService;
+        meterRegistry = registry;
+    }
+
+    @Operation(summary = "Provide the personal access token to retrieve the secure maintainer and developer links")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "secure maintainer and developer links",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = LinkConstruct.class))}),
+            @ApiResponse(responseCode = "403", description = "Forbidden - Requested code does not exist"),
+
+    })
+    @PostMapping("/PAT")
+    public ResponseEntity<LinkConstruct> createFromPAT(@RequestBody PATConstruct patRequest) {
+        LinkConstruct lc = gitRepoService.createAccessLinks(patRequest);
+        meterRegistry.counter("pat_registration_total").increment();
+        return ResponseEntity.ok(lc);
+    }
+
+    @Operation(summary = "Get the git repository information", description = "Auth is handled via the provided UUID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "get repository-content",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = GitRepoInformationConstruct.class))}),
+            @ApiResponse(responseCode = "403", description = "Forbidden - Requested code does not exist",
+                    content = {@Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))}),
+    })
+    @GetMapping("/{usercode}")
+    public ResponseEntity<GitRepoInformationConstruct> getGitRepository(@PathVariable @NotNull UUID usercode) {
+        GitRepoInformationConstruct gitRepository = gitRepoService.getRepositoryByAccessID(usercode);
+        return ResponseEntity.ok(gitRepository);
+    }
+
+    @Operation(summary = "allows searching the repository's content")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "searched repository-content",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = SearchResult.class))}),
+            @ApiResponse(responseCode = "403", description = "Forbidden - Requested code does not exist",
+                    content = {@Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))}),
+
+    })
+    @GetMapping("/{usercode}/search")
+    public ResponseEntity<SearchResult> search(
+            @PathVariable @NotNull UUID usercode,
+            @RequestParam(name = "query") @NotNull @NotBlank String query) {
+        GitRepoInformationConstruct gitRepository = gitRepoService.getRepositoryByAccessID(usercode);
+        if (gitRepository.contents().isEmpty())
+            return ResponseEntity.noContent().build();
+        SearchResult results = searchService.search(query);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(results);
+    }
+
+    @Operation(summary = "tell the AI service which items should be included into the summary")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Items were included in the summary",
+                    content = {@Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid input provided - please make sure that all selected content exists",
+                    content = {@Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))}),
+            @ApiResponse(responseCode = "403", description = "Forbidden - Requested code does not exist",
+                    content = {@Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))}),
+
+    })
+    @PostMapping("/{usercode}/selection")
+    public ResponseEntity<String> createCommitSelectionForSummary(@PathVariable @NotNull UUID usercode, @RequestBody SelectionSubmission selection) {
+        gitRepoService.createCommitSelection(usercode, selection);
+
+        return ResponseEntity.ok("Created Successfully");
+    }
+
+
+    @Operation(summary = "create a question to be answered asynchronously by the ai service")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Items were included in the summary",
+                    content = {@Content(mediaType = "text/plain", schema = @Schema(implementation = String.class))}),
+            @ApiResponse(responseCode = "403", description = "Forbidden - Requested code does not exist"),
+
+    })
+    @PostMapping("/{usercode}/question")
+    public ResponseEntity<String> createQuestion(
+            @PathVariable @NotNull UUID usercode,
+            @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                description = "Question to create",
+                required = true,
+                content = @Content(
+                        mediaType = "text/plain",
+                        schema = @Schema(implementation = QuestionSubmission.class),
+                        examples = @ExampleObject(value = "{ \"question\": \"Why are these developer raving about 42?\" }"))
+                )
+    @RequestBody
+    QuestionSubmission question) {
+        gitRepoService.createQuestion(usercode, question.question());
+        meterRegistry.counter("question_creation_total").increment();
+
+        return ResponseEntity.ok("Created Successfully");
+    }
+}
---
+++ server/src/main/resources/db/migration/V1__create_Intial_schema.sql
@@ -0,0 +1,68 @@
+CREATE TABLE "git_repositories" (
+                                    "id" bigint PRIMARY KEY NOT NULL,
+                                    "repository_link" text UNIQUE NOT NULL,
+                                    "created_at" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "personal_access_tokens" (
+                                          "pat" text PRIMARY KEY NOT NULL,
+                                          "created_at" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "summaries" (
+                             "id" bigint PRIMARY KEY NOT NULL,
+                             "git_repository_id" bigint NOT NULL,
+                             "summary" text NOT NULL,
+                             "created_at" timestamp DEFAULT (now())
+);
+
+CREATE TABLE "contents" (
+                            "id" text PRIMARY KEY NOT NULL,
+                            "git_repository_id" bigint NOT NULL,
+                            "type" text NOT NULL,
+                            "user" text NOT NULL,
+                            "summary" text NOT NULL,
+                            "is_selected" boolean not null default true,
+                            "created_at" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "questions" (
+                             "id" bigint PRIMARY KEY NOT NULL,
+                             "git_repository_id" bigint NOT NULL,
+                             "question" text NOT NULL,
+                             "created_at" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "question_answers" (
+                                    "id" bigint PRIMARY KEY NOT NULL,
+                                    "question_id" bigint NOT NULL,
+                                    "answer" text NOT NULL,
+                                    "created_at" timestamp NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "links" (
+                         "id" uuid PRIMARY KEY NOT NULL,
+                         "is_maintainer" bool NOT NULL,
+                         "git_repository_id" bigint NOT NULL
+);
+
+CREATE TABLE "personal_access_tokens_git_repositories" (
+                                                           "personal_access_tokens_pat" text,
+                                                           "git_repositories_id" bigint,
+                                                           PRIMARY KEY ("personal_access_tokens_pat", "git_repositories_id")
+);
+
+ALTER TABLE "personal_access_tokens_git_repositories" ADD FOREIGN KEY ("personal_access_tokens_pat") REFERENCES "personal_access_tokens" ("pat");
+
+ALTER TABLE "personal_access_tokens_git_repositories" ADD FOREIGN KEY ("git_repositories_id") REFERENCES "git_repositories" ("id");
+
+
+ALTER TABLE "summaries" ADD CONSTRAINT "repo_summaries" FOREIGN KEY ("git_repository_id") REFERENCES "git_repositories" ("id");
+
+ALTER TABLE "contents" ADD CONSTRAINT "repo_contents" FOREIGN KEY ("git_repository_id") REFERENCES "git_repositories" ("id");
+
+ALTER TABLE "questions" ADD CONSTRAINT "repo_questions" FOREIGN KEY ("git_repository_id") REFERENCES "git_repositories" ("id");
+
+ALTER TABLE "question_answers" ADD CONSTRAINT "repo_question" FOREIGN KEY ("question_id") REFERENCES "questions" ("id");
+
+ALTER TABLE "links" ADD CONSTRAINT "repo_links" FOREIGN KEY ("git_repository_id") REFERENCES "git_repositories" ("id");
